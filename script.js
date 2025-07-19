bf50010430d8145ac7581cce35b4b69d0b025b83
paijo <mohammadyahyarudianto@gmail.com>
2025-07-19 20:08:47 +0700

    tambah file

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+let card = document.getElementsByClassName("card");
+
+
+ card[0].addEventListener("click",myAnim );
+ 
+ function myAnim() {
+   alert("ABANDON = MENINGGALKAN");
+ }
+ card[1].addEventListener("click",myAnim1 );
+ 
+ function myAnim1() {
+   alert("WORK = BEKERJA");
+ }
